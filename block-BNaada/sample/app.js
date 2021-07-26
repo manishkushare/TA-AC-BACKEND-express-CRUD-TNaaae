20aba9f04be1f34fbb8ad582a475ed72a343cc01
manishkushare <manishkushare@outlook.com>
2021-07-26 13:24:38 +0530

    ldone

---
+++ block-BNaada/sample/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/user5',(err)=> {
+    console.log(err? err: "Connected to database");
+});
+
+const app= express();
+
+// middlewares
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+
+// routing middlewares
+app.use('/',require('./routes/index'));
+app.use('/users',require('./routes/users'));
+// error handeling middlewares
+app.use((req,res,next)=> {
+    res.send('Page not found');
+})
+app.use((err,req,res,next)=>{
+    res.send(err);
+})
+
+app.listen(4200,()=> {
+    console.log("listening on server 3k");
+})
